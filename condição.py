447e658e0c74621f12d5e803bbd5ff15a0b1e890
Enthony Araújo <enthonyaraujo01@gmail.com>
2023-11-08 18:59:05 -0300

    Create condição.py

---
+++ condição.py
@@ -0,0 +1,9 @@
+#exemplo de condição
+
+a = int(input("Digite um numero: "))
+b = int(input("Digite outro numero: "))
+
+if a > b:
+    print("%d maior que %d" %(a,b))
+if b > a:
+    print("%d maior que %d"%(b,a))
