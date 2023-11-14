600541acd7c51dabe2214de4115d743aaca5367c
Enthony Araújo <enthonyaraujo01@gmail.com>
2023-11-14 09:37:49 -0300

    Update 4.2.py

--- Exercícios/Capitulo 4/4.2.py
+++ Exercícios/Capitulo 4/4.2.py
@@ -3,10 +3,10 @@ usuário. Caso ultrapasse 80 km/h, exiba uma mensagem dizendo que o usuário
 foi multado. Nesse caso, exiba o valor da multa, cobrando R$ 5 por km acima de
 80 km/h.'''
 
-velocidade = int(input("Olá, a que velocidade você estava no carro? "))
+velocidade = int(input("Olá, a que velocidade (km/h) você estava no carro? "))
 multa = velocidade*5
 
 if velocidade > 80:
     print("Você foi multado em %d." %multa)
-else:
+if velocidade <= 80:
     print("Tudo bem, pode seguir viagem.")
