97006bad4c5a9dd098151b4ab6c9f2da3d09b63a
Enthony Araújo <enthonyaraujo01@gmail.com>
2023-11-29 10:15:52 -0300

    add

--- condição.py
+++
@@ -1,9 +0,0 @@
-tabuada = 1
-
-while tabuada <=10:
-    numero = 1
-    print('Tabuada de %d' %tabuada)
-    while numero <=10:
-        print('%d x %d = %d' %(tabuada,numero,numero*tabuada    ))
-        numero +=1
-    tabuada +=1
